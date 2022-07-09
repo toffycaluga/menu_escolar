5ad0103c36ac978c418c44c6b4fe39cf305b4fe7
toffycaluga <95626402+toffycaluga@users.noreply.github.com>
2022-07-09 09:55:35 -0400

    server configurado

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import nunjucks from 'nunjucks';
+import session from 'express-session';
+import flash from 'connect-flash';
+import auth from './routes/auth.js';
+
+
+const app = express();
+
+app.use(express.static('static'));
+
+nunjucks.configure('templates', {
+    express: app,
+    autoscape: true,
+    watch: true,
+});
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'mi-clave',
+    cookie: { maxAge: 100 * 60 * 60 * 24 }
+}));
+
+app.use(flash());
+
+// asignando user como variabe global
+app.use((req, res, next) => {
+    res.locals.user = req.session.user
+    console.log(req.session.user);
+    next()
+});
+// rutas
+
+app.use(auth);
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`servidor en puerto ${PORT}`));
+
